8db0b0460c5873f449d66ad82b7149a640484751
guoqing2013@163.com <qing1211>
2015-11-24 12:30:18 +0800

    note

--- 代码/app/scripts/controllers/couponCtrl.js
+++
@@ -1,6 +0,0 @@
-﻿'use strict';
-module.exports = ['$scope', '$timeout', '$sessionStorage', 'nexus', 'nav', 'loading', 'AJAX', 'dateRange', 'authService', 'selectChecked',
-    function ($scope, $timeout, $sessionStorage, nexus, nav, loading, AJAX, dateRange, authService,  selectChecked) {
-
-    }
-];
